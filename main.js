5653ce87c963d70a3bb4d4eb7a99b3e77c3fc7eb
Aaron Jacobs <aaronj313@gmail.com>
2024-08-29 11:15:18 -0700

    add changes to plugin to get facebook and instagram blocked, and add a button allowing the user to open a new tab to play the google snake game

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+let currentWebsite;
+
 // defines section containing vides on youtube.com
-const youtubeContent = $('#content');
-// defines section containing content on FB
+// const youtubeContent = $('#content');
 
 // defines section containing content on instagram
 const instagramContent = $('.x78zum5.xdt5ytf.x1iyjqo2.xg6iff7')
+
+// defines section containing content on facebook
+const facebookContent = $('.x9f619.x1n2onr6.x1ja2u2z')
 
 // defines section containing content on X
 
@@ -34,9 +38,44 @@ const appendText = '<div id="focusText">Focus on your dream!</div>';
 
 const appendImage = '<img id="displayImage" src="https://upload.wikimedia.org/wikipedia/commons/3/36/Software_Developer_at_work_03.jpg" alt="Focus on your dream!" />'
 
+const asnakeLink = '<a href="https://www.googlesnake.org" id="googles-snake-game">Play Snake Game </a>'
+
 // if URL is youtube, replace youtubeContent with newSection
-instagramContent.replaceWith('<div id="newSection"></div>');
-youtubeContent.replaceWith('<div id="newSection"></div>');
-// if URL is facebook, replace facebookContent with newSection
-$('#newSection').prepend(appendText);
-$('#focusText').after(appendImage);
+
+window.addEventListener('load', () => {
+  currentWebsite = window.location.href
+  console.log(currentWebsite)
+  // if URL is facebook, replace facebookContent with newSection
+  if(currentWebsite.includes('www.instagram.com')){
+    console.log(`it's Instagram`)
+    instagramContent.replaceWith('<div id="newSection"></div>');
+  }
+
+  if(currentWebsite.includes('www.facebook.com')){
+    console.log(`it's Facebook`)
+    facebookContent.replaceWith('<div id="newSection"></div>');
+  }
+
+  $('#newSection').prepend(appendText);
+  $('#focusText').after(appendImage);
+  // $('#displayImage').after(asnakeLink)
+  $('#displayImage').after('<a id="snakeButton" href="https://www.googlesnake.org" target="_blank">Play Snake Game</a>')
+
+  // const snakeLink = document.getElementById('googles-snake-game');
+  // const snakeGameUrl = 'https://www.googlesnake.org';
+
+  // snakeLink.addEventListener('click', (e) => {
+  //   e.preventDefault();
+  //   chrome.tabs.create({ url: snakeGameUrl });
+});
+
+
+
+
+// const snakeLink = document.getElementById('googles-snake-game');
+//   const snakeGameUrl = 'https://www.googlesnake.org';
+
+//   snakeLink.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     chrome.tabs.create({ url: snakeGameUrl });
+// });
